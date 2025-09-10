6436b32d159bb94c230545e611d83bcdd05db361
Himanshu Raj <itshimanshuraaz@gmail.com>
2025-09-10 21:11:22 +0530

    done

--- app/api/chat/get/route.js
+++ app/api/chat/get/route.js
@@ -3,7 +3,7 @@ import Chat from "@/models/Chat";
 import {getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function Get(req) {
+export async function GET(req) {
     try {
         const {userId} = getAuth(req);
 
